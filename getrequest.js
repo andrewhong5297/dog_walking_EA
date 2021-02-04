065e5fd4f53c82b0ce22e6d7c791587308347849
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2021-02-04 15:21:35 -0500

    updated request

--- getrequest.js
+++ getrequest.js
@@ -1,19 +1,33 @@
 `{
-    "initiators": [
-      {
-        "type": "RunLog",
-        "params": { "address": "0x51DE85B0cD5B3684865ECfEedfBAF12777cd0Ff8" }
+  "initiators": [
+    {
+      "type": "runlog",
+      "params": {
+        "address": "0xf5a4036ca35b9c017efa49932dca4bc8cc781aa4"
       }
-    ],
-    "tasks": [
-      {
-        "type": "HTTPpost",
-        "confirmations": 0,
-      },
-      { "type": "EthUint256[]" },
-      { "type": "EthTx" }
-    ],
-    "startAt": "2020-02-09T15:13:03Z",
-    "endAt": null,
-    "minPayment": "1000000000000000000"
-  }`
+    }
+  ],
+  "tasks": [
+    {
+      "type": "openweathermap_cl_ea",
+      "confirmations": 0,
+      "params": {
+        "address": "31803877693398533056304374249487059501607363464"
+      }
+    },
+    {
+      "type": "ethbytes32",
+      "confirmations": null,
+      "params": {
+      }
+    },
+    {
+      "type": "ethtx",
+      "confirmations": null,
+      "params": {
+      }
+    }
+  ],
+  "startAt": "2020-02-09T15:13:03Z",
+  "endAt": null
+}`
